a5421034b4b5e037d6e8b60efa8e7af899646b60
Didod5 <didod1020@gmail.com>
2023-11-20 00:36:11 +0400

    deleted config

--- 3.0/config.py
+++
@@ -1,7 +0,0 @@
-db_params = {
-    'host': 'cornelius.db.elephantsql.com',
-    'port': '5432',
-    'user': 'lexszric',
-    'password': 'Kj98jGXvZAR3QDbU8Z9w7sSKDWgk6Xs_',
-    'database': 'lexszric',
-}
